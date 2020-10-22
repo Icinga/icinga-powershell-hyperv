ca31a66d8090fa51b2818fcea57fe002ffdf23b9
Yonas Habteab <Yonas.Habteab@netways.de>
2020-10-22 15:03:04 +0200

    Add monitoring for virtual switches
    resolves #8

---
+++ doc/plugins/03-Invoke-IcingaCheckHyperVVirtualSwitches.md
@@ -0,0 +1,52 @@
+
+# Invoke-IcingaCheckHyperVVirtualSwitches
+
+## Description
+
+Checks the state of a Hyper-V virtual switch
+
+Invoke-IcingaCheckHyperVVirtualSwitches checks by default all available Hyper-V virtual switches for
+their state, e.g. whether they are still running or the communication between the VM has not been
+interrupted. But you can also use the individual parameters see below to filter out or filter in
+virtual switches that you want to check or not.
+
+## Permissions
+
+To execute this plugin you will require to grant the following user permissions.
+
+### WMI Permissions
+
+* Root\Virtualization\v2
+* Root\Cimv2
+
+### Required User Groups
+
+* Performance Log Users
+* Hyper-V Administrator
+
+## Arguments
+
+| Argument | Type | Required | Default | Description |
+| ---      | ---  | ---      | ---     | ---         |
+| IncludeSwitches | Array | false | @() | With this parameter you can filter virtual switches for the check in. e.g. testswitch1, testswitch1. |
+| ExcludeSwitches | Array | false | @() | With this parameter you can filter out virtual switches for the check. e.g. testswitch1, testswitch1. |
+| InternalSwitches | SwitchParameter | false | False | Only the internal virtual switches are added to the check. |
+| ExternalSwitches | SwitchParameter | false | False | Only the external virtual switches are added to the check. |
+| SwitchStatusWarning | Object | false |  | Warning threshold for Switch Status indicates that an element is functioning properly, but is predicating a failure. |
+| SwitchStatusCritical | Object | false |  | Critical threshold for Switch Status indicates that an element is functioning properly, but is predicating a failure. |
+| NoPerfData | SwitchParameter | false | False | Disables the performance data output of this plugin |
+| Verbosity | Object | false | 0 | Changes the behavior of the plugin output which check states are printed: 0 (default): Only service checks/packages with state not OK will be printed 1: Only services with not OK will be printed including OK checks of affected check packages including Package config 2: Everything will be printed regardless of the check state |
+
+## Examples
+
+### Example Command 1
+
+```powershell
+Invoke-IcingaCheckHyperVVirtualSwitches -Verbosity 2
+```
+
+### Example Output 1
+
+```powershell
+[OK] Check package "Virtual Switches" (Match All)\_ [OK] Check package "Internal Switch" (Match All)\_ [OK] Internal Switch HealthState: OK\_ [OK] Internal Switch Status: OK\_ [OK] Check package "Internal Switch1" (Match All)\_ [OK] Internal Switch1 HealthState: OK\_ [OK] Internal Switch1 Status: OK\_ [OK] Check package "net-hq" (Match All)\_ [OK] net-hq HealthState: OK\_ [OK] net-hq Status: OK\_ [OK] Check package "net-private-test" (Match All)\_ [OK] net-private-test HealthState: OK\_ [OK] net-private-test Status: OK\_ [OK] Check package "sales-demo" (Match All)\_ [OK] sales-demo HealthState: OK\_ [OK] sales-demo Status: OK| 'netprivatetest_healthstate'=5;;25 'salesdemo_healthstate'=5;;25 'nethq_healthstate'=5;;25 'internal_switch_healthstate'=5;;25 'internal_switch1_healthstate'=5;;250
+```
---
+++ plugins/Invoke-IcingaCheckHyperVVirtualSwitches.psm1
@@ -0,0 +1,128 @@
+<#
+.SYNOPSIS
+    Checks the state of a Hyper-V virtual switch
+.DESCRIPTION
+    Invoke-IcingaCheckHyperVVirtualSwitches checks by default all available Hyper-V virtual switches for
+    their state, e.g. whether they are still running or the communication between the VM has not been
+    interrupted. But you can also use the individual parameters see below to filter out or filter in
+    virtual switches that you want to check or not.
+.ROLE
+    ### WMI Permissions
+
+    * Root\Virtualization\v2
+    * Root\Cimv2
+
+    ### Required User Groups
+
+    * Performance Log Users
+    * Hyper-V Administrator
+.PARAMETER IncludeSwitches
+    With this parameter you can filter virtual switches for the check in. e.g. testswitch1, testswitch1.
+.PARAMETER ExcludeSwitches
+    With this parameter you can filter out virtual switches for the check. e.g. testswitch1, testswitch1.
+.PARAMETER InternalSwitches
+    Only the internal virtual switches are added to the check.
+.PARAMETER ExternalSwitches
+    Only the external virtual switches are added to the check.
+.PARAMETER SwitchStatusWarning
+    Warning threshold for Switch Status indicates that an element is functioning properly, but is predicating a failure.
+.PARAMETER SwitchStatusCritical
+    Critical threshold for Switch Status indicates that an element is functioning properly, but is predicating a failure.
+.PARAMETER NoPerfData
+    Disables the performance data output of this plugin
+.PARAMETER Verbosity
+    Changes the behavior of the plugin output which check states are printed:
+    0 (default): Only service checks/packages with state not OK will be printed
+    1: Only services with not OK will be printed including OK checks of affected check packages including Package config
+    2: Everything will be printed regardless of the check state
+.EXAMPLE
+    PS> Invoke-IcingaCheckHyperVVirtualSwitches -Verbosity 2
+    [OK] Check package "Virtual Switches" (Match All)
+    \_ [OK] Check package "Internal Switch" (Match All)
+        \_ [OK] Internal Switch HealthState: OK
+        \_ [OK] Internal Switch Status: OK
+    \_ [OK] Check package "Internal Switch1" (Match All)
+        \_ [OK] Internal Switch1 HealthState: OK
+        \_ [OK] Internal Switch1 Status: OK
+    \_ [OK] Check package "net-hq" (Match All)
+        \_ [OK] net-hq HealthState: OK
+        \_ [OK] net-hq Status: OK
+    \_ [OK] Check package "net-private-test" (Match All)
+        \_ [OK] net-private-test HealthState: OK
+        \_ [OK] net-private-test Status: OK
+    \_ [OK] Check package "sales-demo" (Match All)
+        \_ [OK] sales-demo HealthState: OK
+        \_ [OK] sales-demo Status: OK
+    | 'netprivatetest_healthstate'=5;;25 'salesdemo_healthstate'=5;;25 'nethq_healthstate'=5;;25 'internal_switch_healthstate'=5;;25 'internal_switch1_healthstate'=5;;25
+    0
+.LINK
+    https://github.com/Icinga/icinga-powershell-hyperv
+    https://github.com/Icinga/icinga-powershell-framework
+#>
+function Invoke-IcingaCheckHyperVVirtualSwitches()
+{
+    param(
+        [array]$IncludeSwitches        = @(),
+        [array]$ExcludeSwitches        = @(),
+        [switch]$InternalSwitches      = $FALSE,
+        [switch]$ExternalSwitches      = $FALSE,
+        [ValidateSet('OK', 'Error', 'Degraded', 'Unknown', 'Pred Fail', 'Starting', 'Stopping', 'Service', 'Stressed', 'NonRecover', 'No Contanct', 'Lost Comm')]
+        $SwitchStatusWarning           = $null,
+        [ValidateSet('OK', 'Error', 'Degraded', 'Unknown', 'Pred Fail', 'Starting', 'Stopping', 'Service', 'Stressed', 'NonRecover', 'No Contanct', 'Lost Comm')]
+        $SwitchStatusCritical          = $null,
+        [switch]$NoPerfData            = $FALSE,
+        [ValidateSet(0, 1, 2)]
+        $Verbosity                     = 0
+    );
+
+    # Create a basic CheckPackage, where all following CheckPackages and checks are appended
+    $CheckPackage    = New-IcingaCheckPackage -Name 'Virtual Switches' -OperatorAnd -Verbose $Verbosity;
+    # Get all the necessary information from the provider regarding virtual switches
+    $VirtualSwitches = Get-IcingaHypervVirtualSwitches `
+        -IncludeSwitches $IncludeSwitches `
+        -ExcludeSwitches $ExcludeSwitches `
+        -InternalSwitches:$InternalSwitches `
+        -ExternalSwitches:$ExternalSwitches;
+
+    $LatestSwitch = '';
+    # We loop through all available virtual switches
+    foreach ($switch in $VirtualSwitches.Keys) {
+        $VirtualSwitch = $VirtualSwitches[$switch];
+        $CheckPackageName = $VirtualSwitch.ElementName;
+        if ($LatestSwitch -eq $VirtualSwitch.ElementName) {
+            $CheckPackageName = ([string]::Format('{0}1', $VirtualSwitch.ElementName));
+        }
+
+        $LatestSwitch = $VirtualSwitch.ElementName;
+        # Create a CheckPackage for each individual virtual switch
+        $SwitchCheckPackage = New-IcingaCheckPackage -Name $CheckPackageName -OperatorAnd -Verbose $Verbosity;
+        $SwitchCheckPackage.AddCheck(
+            (
+                New-IcingaCheck `
+                    -Name ([string]::Format('{0} Status', $CheckPackageName)) `
+                    -Value $VirtualSwitch.Status `
+                    -NoPerfData
+            ).WarnIfMatch(
+                $SwitchStatusWarning
+            ).CritIfMatch(
+                $SwitchStatusCritical
+            )
+        );
+
+        $SwitchCheckPackage.AddCheck(
+            (
+                New-IcingaCheck `
+                    -Name ([string]::Format('{0} HealthState', $CheckPackageName)) `
+                    -Value $VirtualSwitch.HealthState `
+                    -Translation $HypervProviderEnums.VMHealthState
+            ).CritIfNotMatch(
+                $HypervProviderEnums.VMHealthStateName.OK
+            )
+        );
+
+        # After each loop we add the single virtual switch CheckPackage to our basic CheckPackage
+        $CheckPackage.AddCheck($SwitchCheckPackage);
+    }
+
+    return (New-IcingaCheckResult -Check $CheckPackage -NoPerfData $NoPerfData -Compile);
+}
